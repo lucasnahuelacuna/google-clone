af763b953a7fa7eebf1e23ddff7191394e49eb07
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2021-04-24 13:10:52 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Google Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Google Clone</h1>
+    </div>
+  )
+}
