cfc089ef8fa706ca857da899b1253851e5e6065a
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2021-04-25 18:52:47 -0300

    added googleapis fetch

---
+++ keys.js
@@ -0,0 +1,5 @@
+//API Key - https://developers.google.com/custom-search/v1/using_rest
+
+//Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY= '6709a3bac03ced09c'
+
--- pages/search.js
+++ pages/search.js
@@ -1,20 +1,43 @@
 import Head from "next/head"
 import Header from "../components/Header"
+import { CONTEXT_KEY } from '../keys'
+import Response from '../Response'
+import { useRouter } from 'next/router'
+import SearchResults from "../components/SearchResults"
 
-const Search = () => {
+const API_KEY = process.env.API_KEY;
+//const CONTEXT_KEY = process.env.CONTEXT_KEY
+
+const Search = ({ results }) => {
+    const router = useRouter();
+    console.log(results)
+
     return (
         <div>
             <Head>
-                <title>Search Results</title>
+                <title>{router.query.term} - Google Search</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            {/* Header */}
             <Header />
 
-            {/* Search Results */}
+            <SearchResults results={results} />
         </div>
     )
 }
 
 export default Search
+
+export async function getServerSideProps(context) {
+    const useDummyData = true;
+    const startIndex = context.query.start || '0'; //Pagination
+
+    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+    ).then(res => res.json())
+    
+    return {
+        props: {
+            results: data,
+        }
+    }
+}
